1703136b4ab4f8b579c429a6a258ddbb5b5a342a
Hugo Novo Presumido <hugonovopresu@gmail.com>
2025-05-27 16:06:18 +0200

    qwe

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,10 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return '✅ Backend Laravel funcionando en Railway';
+});
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
 });
