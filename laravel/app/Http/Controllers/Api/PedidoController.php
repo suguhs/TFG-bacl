a3c139b6c1ae0e4fbc753ed6ce9db585b40ed9f4
Hugo Novo Presumido <hugonovopresu@gmail.com>
2025-05-29 20:07:40 +0200

    qwe

--- laravel/app/Http/Controllers/Api/PedidoController.php
+++ laravel/app/Http/Controllers/Api/PedidoController.php
@@ -55,7 +55,7 @@ class PedidoController extends Controller
     public function updateEstado(Request $request, $id)
     {
         $request->validate([
-            'estado' => 'required|in:pendiente,preparando,enviado,entregado,cancelado'
+            'estado' => 'required|in:pendiente,aceptado,enviado,entregado,cancelado'
         ]);
 
         $pedido = Pedido::findOrFail($id);
